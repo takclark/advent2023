cc2595c8c9140069ca3e69054951d01665e64b33
Tyler Clark <tyler@justworkslabs.com>
2023-12-04 16:50:55 -0500

    a messy pt 2

--- lib/04/Advent202304.ex
+++ lib/04/Advent202304.ex
@@ -9,8 +9,79 @@ defmodule Advent202304 do
   end
 
   def solve_02(filename) do
-    # initialize a map with process counts, 1 for all rows to start
     AdventUtils.load_file(filename)
+    |> Enum.map(fn x ->
+      {x, 1, count_matches(x)}
+    end)
+    |> process
+  end
+
+  def process(chart) do
+    case chart do
+      # base case, we're done
+      [] ->
+        0
+
+      _ ->
+        left_for_this_line = elem(hd(chart), 1)
+
+        case left_for_this_line do
+          0 ->
+            IO.puts("finished with:")
+            IO.puts("#{elem(hd(chart), 0)}")
+            IO.puts("next n:")
+
+            unless tl(chart) == [] do
+              IO.puts("#{elem(hd(tl(chart)), 1)}")
+            end
+
+            process(tl(chart))
+
+          _ ->
+            win_count = elem(hd(chart), 2)
+
+            1 +
+              process([
+                put_elem(hd(chart), 1, left_for_this_line - 1)
+                | carry(tl(chart), win_count)
+              ])
+        end
+    end
+  end
+
+  @doc """
+  Apply a "winning" count n forward through chart, a list of tuples {ticket, number of times to process}
+  """
+  def carry(chart, n) do
+    case chart do
+      [] ->
+        chart
+
+      _ ->
+        if n == 0 do
+          chart
+        else
+          [put_elem(hd(chart), 1, elem(hd(chart), 1) + 1) | carry(tl(chart), n - 1)]
+        end
+    end
+  end
+
+  def count_matches(row_str) do
+    winners =
+      row_str
+      |> String.split("|")
+      |> hd
+      |> String.split(" ")
+      |> Enum.filter(fn x -> AdventUtils.string_parses(x) end)
+
+    yours =
+      row_str
+      |> String.split("|")
+      |> List.last()
+      |> String.split(" ")
+      |> Enum.filter(fn x -> AdventUtils.string_parses(x) end)
+
+    score_matches(winners, yours)
   end
 
   def score_row(row_str) do
