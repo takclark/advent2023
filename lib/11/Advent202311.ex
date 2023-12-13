16c04917d163481ed2f852bc4f512e375a205367
Tyler Clark <tyler@justworkslabs.com>
2023-12-13 16:15:42 -0500

    11 pt. 2 + refactor

--- lib/11/Advent202311.ex
+++ lib/11/Advent202311.ex
@@ -1,9 +1,19 @@
 defmodule Eleven do
+  require IEx
   def solve_01(filename) do
     {_, content} = File.read(filename)
-    map = content |> String.split("\n") |> expand
-    map |> galaxy_coords |> distances
+    map = content |> String.split("\n")
+    map |> galaxy_coords |> expand_coords(col_ndxs_to_expand(map), row_ndxs_to_expand(map), 1) |> distances
   end
+
+  def solve_02(filename) do
+    {_, content} = File.read(filename)
+    map = content |> String.split("\n")
+    map |> galaxy_coords |> expand_coords(col_ndxs_to_expand(map), row_ndxs_to_expand(map), 999999) |> distances
+  end
+
+  # distance is simple enough on a grid, we're counting steps and not really "distance"
+  def dist(a, b), do: abs(elem(b, 0) - elem(a, 0)) + abs(elem(b, 1) - elem(a, 1))
 
   def distances([]), do: 0
   def distances([_]), do: 0
@@ -11,34 +21,31 @@ defmodule Eleven do
     (coord_list |> tl |> Enum.reduce(0, fn x, acc -> acc + dist(hd(coord_list), x) end)) + (coord_list |> tl |> distances)
   end
 
-  def expand(rows) do
-    rows |> expand_rows |> expand_cols
+  def row_ndxs_to_expand(rows) do
+    rows |> Enum.with_index |> Enum.reduce([], fn x, acc ->
+      if Regex.match?(~r/[^.]/, elem(x, 0)) do acc else [elem(x, 1) | acc] end
+    end)
   end
 
-  # row dimension: succicnt, elegant
-  @spec expand_rows(any()) :: list()
-  def expand_rows(rows) do
-    rows |> Enum.map(fn x -> if Regex.match?(~r/[^.]/, x) do x else [x, x] end end) |> List.flatten
-  end
-
-  # col dimension: you fool, you utter baffoon, good day to be an electric company
-  def expand_cols(rows) do
+  def col_ndxs_to_expand(rows) do
     gphs = rows |> Enum.map(fn x -> String.graphemes(x) end)
-    cols_to_expand = 0..(gphs |> hd |> Enum.count) - 1 |> Enum.filter(fn n ->
+    0..(gphs |> hd |> Enum.count) - 1 |> Enum.filter(fn n ->
       Enum.all?(gphs, fn x ->
         Enum.at(x, n) == "."
       end)
     end)
+  end
 
-    gphs |> Enum.map(fn x ->
-      x |> Enum.with_index |> Enum.map(fn c ->
-        if elem(c, 1) in cols_to_expand do
-          [elem(c, 0), elem(c, 0)]
-        else
-          elem(c, 0)
-        end
-      end) |> List.flatten
-      |> Enum.join("") # convert back to string row to match input format of expand?
+  def expand_coords(coord_list, cols, rows, offset) do
+    coord_list |> Enum.map(fn c ->
+      col_offset = cols |> Enum.reduce(0, fn x, acc ->
+        if elem(c, 0) > x do acc + offset else acc end
+      end)
+      row_offset = rows |> Enum.reduce(0, fn y, acc ->
+        if elem(c, 1) > y do acc + offset else acc end
+      end)
+
+      {elem(c, 0) + col_offset, elem(c, 1) + row_offset}
     end)
   end
 
@@ -46,14 +53,5 @@ defmodule Eleven do
     map |> Enum.with_index |> Enum.map(fn row ->
       Regex.scan(~r/#/, elem(row, 0), return: :index) |> List.flatten |> Enum.map(fn x -> {elem(x,0), elem(row,1)} end)
     end) |> Enum.reject(&(&1 == [])) |> List.flatten
-  end
-
-
-  # distance is simple enough on a grid, we're counting steps and not really "distance"
-  def dist(a, b), do: abs(elem(b, 0) - elem(a, 0)) + abs(elem(b, 1) - elem(a, 1))
-
-  def solve_02(filename) do
-    {_, content} = File.read(filename)
-    content
   end
 end
